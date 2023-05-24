2dee8ee6ebf597fadfabc377a551c14e6baa0da0
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2023-05-24 17:37:07 +0100

    some css additions on about page

--- portfolio-react-lq/src/pages/navbar/about.js
+++ portfolio-react-lq/src/pages/navbar/about.js
@@ -5,18 +5,23 @@ const About = () => {
     return (
         <>
             <h2>About</h2>
-            
-            <h5>Lewis Quinn is a director, photographer and videographer based in Edinburgh, Scotland. Specialising in adventure-lifestyle and landscape photography, Lewis has worked with clients across the UK, Europe, and the USA. From humble beginnings photographing trips as an enthusiastic teenager, his work has come a long way. </h5>
+            <div className="aboutWrapper">
+                <img src="https://cdn.myportfolio.com/2b1b8b85-ad36-43bf-93d1-06269f942d23/c850ba06-6a4e-453c-9908-9fbc52d07144_rw_1200.jpg?h=fe087213fe386aba6b0cd07ecccfb427" id="aboutImg" />
 
-            <h5>Lewis creates emotion invoking imagery through the use of his extensive inventory of professional equipment. He captures stunning pictures and videos with Mirrorless cameras and UAVs/drones. The media is edited with a clean, consistent style, and can be tailored to your branding.</h5>
+                <article>
+                    <p id="firstPara">Lewis Quinn is a director, photographer and videographer based in Edinburgh, Scotland. Specialising in adventure-lifestyle and landscape photography, Lewis has worked with clients across the UK, Europe, and the USA. From humble beginnings photographing trips as an enthusiastic teenager, his work has come a long way. </p>
 
-            <h5>The journey of producing visual media has never been easier, thanks to Lewis' experience of building Pivotal | the visual media company. Founded in 2020, Pivotal can handle clients campaigns from start to finish.</h5>
+                    <p>Lewis creates emotion invoking imagery through the use of his extensive inventory of professional equipment. He captures stunning pictures and videos with Mirrorless cameras and UAVs/drones. The media is edited with a clean, consistent style, and can be tailored to your branding.</p>
 
-            <h5>Starting with Pre-Production, Lewis holds scoping sessions with clients to immerse himself with your project and culture. Original, engaging stories are created that will captivate and move your audience. Lewis can lead and work with teams to create the media, before handling post-production editing. Ultimately delivering a quality final product.</h5>
+                    <p>The journey of producing visual media has never been easier, thanks to Lewis' experience of building Pivotal | the visual media company. Founded in 2020, Pivotal can handle clients campaigns from start to finish.</p>
 
-            <h5>To find out more about Lewis, his work, and to keep up with his day to day adventures, why not check out his Instagram?  @LEWISQVINN</h5>
+                    <p>Starting with Pre-Production, Lewis holds scoping sessions with clients to immerse himself with your project and culture. Original, engaging stories are created that will captivate and move your audience. Lewis can lead and work with teams to create the media, before handling post-production editing. Ultimately delivering a quality final product.</p>
 
-            <h5>Say hello! Whatever your ideas, I'd would love to hear them! Get in touch via phone, or the email and social media links below. Feel free to contact me for enquiries, collaborations, or even just a chat - I'd love to grab a coffee! </h5>
+                    <p>To find out more about Lewis, his work, and to keep up with his day to day adventures, why not check out his Instagram?  @LEWISQVINN</p>
+
+                    <p>Say hello! Whatever your ideas, I'd would love to hear them! Get in touch via phone, or the email and social media links below. Feel free to contact me for enquiries, collaborations, or even just a chat - I'd love to grab a coffee! </p>
+                </article>
+            </div>
         </>
     )
 }
