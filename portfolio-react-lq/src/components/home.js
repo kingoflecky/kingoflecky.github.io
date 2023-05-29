e5f1ecc4c9a0f06617b2793280185eaa724af484
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2023-05-29 14:26:14 +0100

    homepage image change

--- portfolio-react-lq/src/components/home.js
+++ portfolio-react-lq/src/components/home.js
@@ -3,8 +3,7 @@ import '../css/home.css'
 function Homepage(props) {
     return (
         <>
-         <h5>here is a picture of me in the wild</h5>
-         <img className="imgLrg" src="https://cdn.myportfolio.com/2b1b8b85-ad36-43bf-93d1-06269f942d23/5235d00a-6a95-4a69-a6d3-797a26d7300e_rw_1200.jpg?h=b8a56f76c3bcb4de00ebfbbb24310ece"></img>
+         <img className="imgLrg" src="https://cdn.myportfolio.com/2b1b8b85-ad36-43bf-93d1-06269f942d23/d7ad3edf-2c92-480d-97b4-adc336975223_rw_3840.jpg?h=4c634502c8dd857e246283f8c3c234cb"></img>
 
 
         </>
