9c2b83ab76c75539bfe12848ccc0c7a9d2c4abae
Lewis Quinn <lewis.quinn@sainsburysbank.co.uk>
2023-06-06 17:12:13 +0100

    fix lint issues

--- src/pages/navbar/portfolio.js
+++ src/pages/navbar/portfolio.js
@@ -6,10 +6,10 @@ function Portfolio() {
         <div className="portfolioGridContainer">
             <div className="portfolioMenu">
 
-                <a className="portfolioMenuItem" id="landscapeItm"><h2>Photography: Landscape and Travel</h2></a>
-                <a className="portfolioMenuItem" id="peopleItm"><h2>Photography: People and Lifestyle</h2></a>
-                <a className="portfolioMenuItem" id="commercialItm"><h2>Photography: Commercial</h2></a>
-                <a className="portfolioMenuItem" id="videographyItm"><h2>Videography</h2></a>
+                <a href="#landscape" className="portfolioMenuItem" id="landscapeItm"><h2>Photography: Landscape and Travel</h2></a>
+                <a href="#people" className="portfolioMenuItem" id="peopleItm"><h2>Photography: People and Lifestyle</h2></a>
+                <a href="#commercial" className="portfolioMenuItem" id="commercialItm"><h2>Photography: Commercial</h2></a>
+                <a href="#video" className="portfolioMenuItem" id="videographyItm"><h2>Videography</h2></a>
             </div>
         </div>
     )
